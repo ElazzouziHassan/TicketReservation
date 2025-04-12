d0ea14d8bc63132fb17a8ae5e18048246dec8408
Wizardy <wizardhassan99@gmail.com>
2025-04-12 15:29:01 +0100

    commit

--- src/main/java/com/ticketservice/Main.java
+++ src/main/java/com/ticketservice/Main.java
@@ -3,35 +3,21 @@ package com.ticketservice;
 import com.ticketservice.service.BilletService;
 import com.ticketservice.service.ReservationService;
 import com.ticketservice.service.UtilisateurService;
-import com.ticketservice.repository.DatabaseConnection;
 import javax.xml.ws.Endpoint;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println("Démarrage de l'application...");
-            
-            // Vérifier la connexion à la base de données
-            System.out.println("Test de la connexion à la base de données...");
-            DatabaseConnection.getEntityManager().close();
-            System.out.println("Connexion à la base de données réussie.");
-            
-            // Publier les services SOAP
-            String baseAddress = "http://0.0.0.0:8181/";
-            
-            Endpoint.publish(baseAddress + "BilletService", new BilletService());
-            System.out.println("BilletService publié à : " + baseAddress + "BilletService?wsdl");
-            
-            Endpoint.publish(baseAddress + "ReservationService", new ReservationService());
-            System.out.println("ReservationService publié à : " + baseAddress + "ReservationService?wsdl");
-            
-            Endpoint.publish(baseAddress + "UtilisateurService", new UtilisateurService());
-            System.out.println("UtilisateurService publié à : " + baseAddress + "UtilisateurService?wsdl");
-            
-            System.out.println("Services SOAP démarrés. Appuyez sur Ctrl+C pour arrêter.");
-        } catch (Exception e) {
-            System.err.println("Erreur lors du démarrage de l'application:");
-            e.printStackTrace();
-        }
+        String baseAddress = "http://0.0.0.0:8181/";
+        
+        Endpoint.publish(baseAddress + "BilletService", new BilletService());
+        System.out.println("BilletService publié à : " + baseAddress + "BilletService?wsdl");
+        
+        Endpoint.publish(baseAddress + "ReservationService", new ReservationService());
+        System.out.println("ReservationService publié à : " + baseAddress + "ReservationService?wsdl");
+        
+        Endpoint.publish(baseAddress + "UtilisateurService", new UtilisateurService());
+        System.out.println("UtilisateurService publié à : " + baseAddress + "UtilisateurService?wsdl");
+        
+        System.out.println("Services SOAP démarrés. Appuyez sur Ctrl+C pour arrêter.");
     }
 }
